06e70e8757d92a0bcb70638d6bb68daaaa10d6cc
Rafael Braga <rafael.m.braga@hotmail.com>
2021-06-26 09:52:48 +0200

    Adding DriverFactory.

---
+++ src/main/java/com/factory/DriverFactory.java
@@ -0,0 +1,37 @@
+package com.factory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    public WebDriver initDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                tlDriver.set(new ChromeDriver());
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                tlDriver.set(new FirefoxDriver());
+                break;
+            default:
+                System.out.println("Browser -> " + browser + "selecionado nao encontrado");
+        }
+
+        getDriver().manage().deleteAllCookies();
+        getDriver().manage().window().maximize();
+        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        return getDriver();
+    }
+
+    public static synchronized WebDriver getDriver() {
+        return tlDriver.get();
+    }
+}
